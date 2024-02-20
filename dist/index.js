b09a7b3e01153f0573f997f526d27bc5531fa80d
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2024-02-20 22:00:04 +0800

    Added redemption-data.csv

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,49 @@
 "use strict";
-console.log(123);
-const addNumber = (a, b) => {
-    return a + b;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-addNumber(1, 2);
-addNumber(3, 5);
-console.log(Date.now());
+Object.defineProperty(exports, "__esModule", { value: true });
+const csv_parser_1 = __importDefault(require("csv-parser"));
+const fs_1 = __importDefault(require("fs"));
+// This function returns false if staff does not exists
+function staffExists(filePath, staffId) {
+    return new Promise((resolve, reject) => {
+        // Results array to store the parsed CSV data
+        const results = [];
+        // Initialize a readable stream to read data from the CSV file
+        const stream = fs_1.default.createReadStream(filePath);
+        // Transform the stream into readable format
+        stream.pipe((0, csv_parser_1.default)())
+            // Event handler for each row of data parsed
+            .on('data', (data) => {
+            // Process each row of data by pushing it into an array
+            results.push(data);
+        })
+            // Event handler when parsing is complete
+            .on('end', () => {
+            // Check if any of the elements in the results array matches the staffId
+            const found = results.some(element => element.staff_pass_id === staffId);
+            resolve(found);
+        })
+            // Catch and handle error if it occurs
+            .on('error', (error) => {
+            console.error('Error parsing the file!', error);
+            reject(error);
+        });
+    });
+}
+// function canRedeem()
+// const staffId1 = "MANAGER_T999888420B"
+// const pathName = "assests/staff-id-to-team-mapping.csv"
+const staffId = "BOSS_4QXV76PK8MM0";
+const pathName = "assests/staff-id-to-team-mapping-long.csv";
+staffExists(pathName, staffId).then(exists => {
+    if (exists) {
+        console.log("Staff Exists");
+    }
+    else {
+        console.log("Staff don't exist");
+    }
+}).catch(error => {
+    console.error('Error:', error);
+});
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,58 @@
-console.log(123);
+import csvParser from 'csv-parser';
+import fs, { existsSync } from 'fs';
+import path from 'path';
 
-const addNumber = (a: number, b: number) => {
-    return a + b;
+
+// Define the structure of each row in the CSV file
+interface CSVRow {
+    staff_pass_id: string;
+    team_name: string;
+    created_at: string;
 }
 
-addNumber(1, 2);
-addNumber(3, 5);
-console.log(Date.now());
+// This function returns false if staff does not exists
+function staffExists(filePath: string, staffId: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+        // Results array to store the parsed CSV data
+        const results: CSVRow[] = [];
+        
+        // Initialize a readable stream to read data from the CSV file
+        const stream = fs.createReadStream(filePath);
+        
+        // Transform the stream into readable format
+        stream.pipe(csvParser())
+        // Event handler for each row of data parsed
+        .on('data', (data: CSVRow) => {
+            // Process each row of data by pushing it into an array
+            results.push(data);
+        })
+        // Event handler when parsing is complete
+        .on('end', () => {
+            // Check if any of the elements in the results array matches the staffId
+            const found = results.some(element => element.staff_pass_id === staffId);
+            resolve(found);
+        })
+        // Catch and handle error if it occurs
+        .on('error', (error: Error) => {
+            console.error('Error parsing the file!', error);
+            reject(error);
+        });
+    });
+}
+
+// function canRedeem()
+
+// const staffId1 = "MANAGER_T999888420B"
+// const pathName = "assests/staff-id-to-team-mapping.csv"
+const staffId = "BOSS_4QXV76PK8MM0"
+const pathName = "assests/staff-id-to-team-mapping-long.csv"
+
+staffExists(pathName, staffId).then(exists => {
+    if (exists) {
+        console.log("Staff Exists");
+    } else {
+        console.log("Staff don't exist");
+    }
+}).catch(error => {
+    console.error('Error:', error);
+});
