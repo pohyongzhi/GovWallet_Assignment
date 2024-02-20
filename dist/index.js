ad6f3a997f636a447a77ac1a1cb134e2457c5c7a
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2024-02-20 16:55:29 +0800

    Completed basic environment setup

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log(123);
+const addNumber = (a, b) => {
+    return a + b;
+};
+addNumber(1, 2);
+addNumber(3, 5);
+console.log(Date.now());
---
+++ server/index.ts
@@ -0,0 +1,9 @@
+console.log(123);
+
+const addNumber = (a: number, b: number) => {
+    return a + b;
+}
+
+addNumber(1, 2);
+addNumber(3, 5);
+console.log(Date.now());
